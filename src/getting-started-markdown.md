3adc7620fbf7cf1fc36bdb91e368d5841df1da1d
cristi <cristi@localhost.localdomain>
2024-01-11 14:01:51 +0200

    chore: update markdown content

--- src/getting-started-markdown.md
+++ src/getting-started-markdown.md
@@ -1,13 +1,26 @@
-## Starphish: Your Personal Phishing Guardian
+## Personalized phishing protection
 
-Recognizing authenticity in unfamiliar contexts: that's the key challenge of phishing detection. Like a user, an anti-phishing system struggles to determine the legitimacy of an unknown authentication page. Is this trustworthy or is it a phishing attempt?
+---
 
-It's hard to verify the legitimacy of a page without some basic understanding of the brand's legitimate URL or webpage design. That's why we developed Starphish to mimic a kind of "familiarity" or knowledge base. It's like the knowledge a human user has when they are familiar with a brand's URL or logo. This insight led us to develop the configuration system. Here, rule sets create a profile for each brand, helping to determine authenticity. This approach allows Starphish to identify phishing attempts with personalization and accuracy.
+Starphish benefits from a **flexible configuration system**, allowing users to register and manage trusted brands of their own liking.
 
-### The Power of Configurations
+Let's say you only access `Bank X`, browse `Store Y` and talk on `Site Z`. These could be world-famous platforms, or some obscure, local
+companies.
 
-The "configurations" are the secret sauce of Starphish. They are user-defined collections of "rules." Each rule includes the brand's metadata, a screenshot of its authentication webpage, the webpage's title, and its favicon. This data creates a detailed profile that Starphish uses to validate websites and detect potential phishing attacks. Users can create and manage different active configurations according to their needs, enhancing the protection offered.
+For a protection custom tailored for you, you could create a **configuration**, called _Personal_, in which you would
+include all 3 platforms. This allows Starphish to get to "know" and recognize the brand, its authenticity, sort of like a fingerprint
+of the website. Now, no other service could pretend to be any of those brands.
 
-This flexibility is vital. Users often interact with various brands across multiple industries, each with unique phishing risks. Starphish includes pre-built public configurations to protect against the most common phishing targets. These primarily safeguard the banking and social media sectors. These sectors are frequent targets of phishing attempts, so these default options provide critical protection.
+Obviously, there are some public configurations, containing some of the most popular services, but the magic of Starphish
+lies in the extent of your personalization.
 
-Remember, a configuration can have multiple rules, and a rule can belong to one or more configurations. It's a many-to-many relationship. This approach offers greater flexibility and increased protection, making Starphish an effective shield against phishing.
+## Comprehensive scan
+
+---
+
+When a scan is requested via the extension, a lot the website's underlying code is analyzed. The _URL_, _HTML_ and
+page's public records are simply not enough. We make use of the Debugger API in order to get a better look into the
+page's intentions.
+
+Even without any active configurations, a plain Starphish scan should recognize phishing intent for most malicious
+websites.
--- src/screens/GettingStarted/index.tsx
+++ src/screens/GettingStarted/index.tsx
@@ -1,15 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
 // @ts-ignore
-import gettingStartedMarkdown from '../../getting-started-markdown.md';
-import { Box, Heading } from "@chakra-ui/react";
+import gettingStartedMarkdown from "../../getting-started-markdown.md";
+import {
+  Box,
+  Button,
+  Heading,
+  Tag,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { useActions } from "../../hooks/useActions";
 import env from "../../env";
+import { Link } from "react-router-dom";
 
 export const GettingStarted = () => {
   const { setActions } = useActions();
 
-  const [markdownContent, setMarkdownContent] = useState('');
+  const [markdownContent, setMarkdownContent] = useState("");
+
+  const accentTextColor = useColorModeValue("secondary", "gray.400");
 
   useEffect(() => {
     getMarkdownContent();
@@ -18,9 +28,9 @@ export const GettingStarted = () => {
 
   const setContextActions = () => {
     setActions({
-      'Download': () => window.open(env.chromeStoreUrl, '_blank'),
-    })
-  }
+      Download: () => window.open(env.chromeStoreUrl, "_blank"),
+    });
+  };
 
   const getMarkdownContent = async () => {
     try {
@@ -28,34 +38,80 @@ export const GettingStarted = () => {
       const content = await response.text();
       setMarkdownContent(content);
     } catch (error) {
-      console.error('Error fetching markdown content:', error);
+      console.error("Error fetching markdown content:", error);
     }
-  }
+  };
 
   const components = {
     h2: ({ children }: { children: any }) => (
-        <Heading as="h2" size="lg" mb="4" fontFamily={'Lato'}>
-          {children}
-        </Heading>
+      <Heading
+        as="h2"
+        size="lg"
+        mb="4"
+        fontFamily={"Lato"}
+        color={accentTextColor}
+      >
+        {children}
+      </Heading>
     ),
     h3: ({ children }: { children: any }) => (
-        <Heading as="h3" size="md" mb="4">
-          {children}
-        </Heading>
+      <Heading as="h3" size="md" mb="4">
+        {children}
+      </Heading>
     ),
-    p: ({ children }: { children: any }) => <p style={{ marginBottom: '1rem' }}>{children}</p>,
+    p: ({ children }: { children: any }) => (
+      <p
+        style={{ marginBottom: "1rem", fontFamily: "Lato", fontSize: "1.2rem" }}
+      >
+        {children}
+      </p>
+    ),
+    ul: ({ children }: { children: any }) => (
+      <ul style={{ marginBottom: "1rem" }}>{children}</ul>
+    ),
+    ol: ({ children }: { children: any }) => (
+      <ol style={{ marginBottom: "1rem", marginLeft: "1rem" }}>{children}</ol>
+    ),
+    li: ({ children }: { children: any }) => (
+      <li style={{ marginBottom: "0.5rem" }}>{children}</li>
+    ),
+    code: ({ children }: { children: any }) => <Tag>{children}</Tag>,
+    strong: ({ children }: { children: any }) => (
+      <strong
+        style={{ marginBottom: "0.5rem", color: accentTextColor }}
+        color={accentTextColor}
+      >
+        {children}
+      </strong>
+    ),
+    em: ({ children }: { children: any }) => (
+      <em style={{ marginBottom: "0.5rem" }}>{children}</em>
+    ),
+    hr: () => <hr style={{ marginBottom: "1rem" }} />,
   };
 
   return (
-      <Box
-          w={'95%'}
-          p={3}
-          pl={6}
-          overflowY="auto"
-          height="100vh"
-          flex="0 0 100%"
-      >
-        <ReactMarkdown children={markdownContent} components={components} />
-      </Box>
-  )
+    <Box w={"95%"} p={3} pl={6} overflowY="auto" height="100vh" flex="0 0 100%">
+      <ReactMarkdown children={markdownContent} components={components} />
+      <Link to={"/configurations"}>
+        <Button colorScheme="teal" variant="link">
+          Get started
+        </Button>
+      </Link>
+      {/* <button */}
+      {/*   style={{ */}
+      {/*     backgroundColor: "#f5f5f5", */}
+      {/*     borderRadius: "0.2rem", */}
+      {/*     padding: "0.5rem", */}
+      {/*     border: "none", */}
+      {/*     fontFamily: "Lato", */}
+      {/*     fontSize: "1.2rem", */}
+      {/*     cursor: "pointer", */}
+      {/*     color: "#000", */}
+      {/*   }} */}
+      {/* > */}
+      {/*   Get started */}
+      {/* </button> */}
+    </Box>
+  );
 };
