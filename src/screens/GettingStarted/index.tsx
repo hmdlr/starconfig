17894359759b4517c6ce9c5e72d0a213be6b3156
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2024-01-15 15:30:12 +0200

    refactor GettingStarted page

--- src/screens/GettingStarted/index.tsx
+++ src/screens/GettingStarted/index.tsx
@@ -1,15 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import ReactMarkdown from "react-markdown";
 // @ts-ignore
 import gettingStartedMarkdown from "../../getting-started-markdown.md";
-import {
-  Box,
-  Button,
-  Heading,
-  Tag,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, Button, Heading, Tag, useColorModeValue } from "@chakra-ui/react";
 import { useActions } from "../../hooks/useActions";
 import env from "../../env";
 import { Link } from "react-router-dom";
@@ -21,18 +14,13 @@ export const GettingStarted = () => {
 
   const accentTextColor = useColorModeValue("secondary", "gray.400");
 
-  useEffect(() => {
-    getMarkdownContent();
-    setContextActions();
-  }, []);
-
-  const setContextActions = () => {
+  const setContextActions = useCallback(() => {
     setActions({
       Download: () => window.open(env.chromeStoreUrl, "_blank"),
     });
-  };
+  }, [setActions]);
 
-  const getMarkdownContent = async () => {
+  const getMarkdownContent = useCallback(async () => {
     try {
       const response = await fetch(gettingStartedMarkdown);
       const content = await response.text();
@@ -40,55 +28,70 @@ export const GettingStarted = () => {
     } catch (error) {
       console.error("Error fetching markdown content:", error);
     }
-  };
+  }, []);
 
-  const components = {
-    h2: ({ children }: { children: any }) => (
-      <Heading
-        as="h2"
-        size="lg"
-        mb="4"
-        fontFamily={"Lato"}
-        color={accentTextColor}
-      >
-        {children}
-      </Heading>
-    ),
-    h3: ({ children }: { children: any }) => (
-      <Heading as="h3" size="md" mb="4">
-        {children}
-      </Heading>
-    ),
-    p: ({ children }: { children: any }) => (
-      <p
-        style={{ marginBottom: "1rem", fontFamily: "Lato", fontSize: "1.2rem" }}
-      >
-        {children}
-      </p>
-    ),
-    ul: ({ children }: { children: any }) => (
-      <ul style={{ marginBottom: "1rem" }}>{children}</ul>
-    ),
-    ol: ({ children }: { children: any }) => (
-      <ol style={{ marginBottom: "1rem", marginLeft: "1rem" }}>{children}</ol>
-    ),
-    li: ({ children }: { children: any }) => (
-      <li style={{ marginBottom: "0.5rem" }}>{children}</li>
-    ),
-    code: ({ children }: { children: any }) => <Tag>{children}</Tag>,
-    strong: ({ children }: { children: any }) => (
-      <strong
-        style={{ marginBottom: "0.5rem", color: accentTextColor }}
-        color={accentTextColor}
-      >
-        {children}
-      </strong>
-    ),
-    em: ({ children }: { children: any }) => (
-      <em style={{ marginBottom: "0.5rem" }}>{children}</em>
-    ),
-    hr: () => <hr style={{ marginBottom: "1rem" }} />,
-  };
+  const components = useMemo(
+    () => ({
+      h2: ({ children }: { children: any }) => (
+        <Heading
+          as="h2"
+          size="lg"
+          mb="4"
+          fontFamily={"Lato"}
+          color={accentTextColor}
+        >
+          {children}
+        </Heading>
+      ),
+      h3: ({ children }: { children: any }) => (
+        <Heading as="h3" size="md" mb="4">
+          {children}
+        </Heading>
+      ),
+      p: ({ children }: { children: any }) => (
+        <p
+          style={{
+            marginBottom: "1rem",
+            fontFamily: "Lato",
+            fontSize: "1.2rem",
+          }}
+        >
+          {children}
+        </p>
+      ),
+      ul: ({ children }: { children: any }) => (
+        <ul style={{ marginBottom: "1rem" }}>{children}</ul>
+      ),
+      ol: ({ children }: { children: any }) => (
+        <ol style={{ marginBottom: "1rem", marginLeft: "1rem" }}>{children}</ol>
+      ),
+      li: ({ children }: { children: any }) => (
+        <li style={{ marginBottom: "0.5rem" }}>{children}</li>
+      ),
+      code: ({ children }: { children: any }) => <Tag>{children}</Tag>,
+      strong: ({ children }: { children: any }) => (
+        <strong
+          style={{ marginBottom: "0.5rem", color: accentTextColor }}
+          color={accentTextColor}
+        >
+          {children}
+        </strong>
+      ),
+      em: ({ children }: { children: any }) => (
+        <em style={{ marginBottom: "0.5rem" }}>{children}</em>
+      ),
+      hr: () => <hr style={{ marginBottom: "1rem" }} />,
+    }),
+    [accentTextColor],
+  );
+
+  useEffect(() => {
+    setContextActions();
+  }, [setContextActions]);
+
+  useEffect(() => {
+    getMarkdownContent();
+  }, [getMarkdownContent]);
 
   return (
     <Box w={"95%"} p={3} pl={6} overflowY="auto" height="100vh" flex="0 0 100%">
@@ -98,20 +101,6 @@ export const GettingStarted = () => {
           Get started
         </Button>
       </Link>
-      {/* <button */}
-      {/*   style={{ */}
-      {/*     backgroundColor: "#f5f5f5", */}
-      {/*     borderRadius: "0.2rem", */}
-      {/*     padding: "0.5rem", */}
-      {/*     border: "none", */}
-      {/*     fontFamily: "Lato", */}
-      {/*     fontSize: "1.2rem", */}
-      {/*     cursor: "pointer", */}
-      {/*     color: "#000", */}
-      {/*   }} */}
-      {/* > */}
-      {/*   Get started */}
-      {/* </button> */}
     </Box>
   );
 };
