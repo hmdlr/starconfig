2ebf4a0b231d4c8ab481ace9de4d370f7c4db478
cristi <cristi@localhost.localdomain>
2024-01-17 17:44:48 +0200

    design improvements

--- src/components/Sidebars/RightSidebar.tsx
+++ src/components/Sidebars/RightSidebar.tsx
@@ -5,27 +5,24 @@ export const RightSidebar = () => {
   const { actions } = useActions();
 
   return (
-      <Box
-          w={{ base: "0", md: "20%" }}
-          borderLeftWidth="thin"
-          borderColor="#e3e3e3"
-          p={3}
-          overflowY="auto"
-          flex="0 0 20%"
-          display={{ base: "none", md: "flex" }}
-          alignItems="center"
-          flexDirection="column"
-      >
-        <Heading as="h2" size="lg" mb="4" textAlign="center">
-          Available Actions
-        </Heading>
-        <VStack spacing={4} align="stretch" width="100%">
-          {Object.entries(actions).map(([actionName, actionFunc], index) => (
-              <Button key={index} onClick={() => actionFunc()}>
-                {actionName}
-              </Button>
-          ))}
-        </VStack>
-      </Box>
+    <Box
+      w={{ base: "0", md: "20%" }}
+      // borderLeftWidth="thin"
+      // borderColor="#e3e3e3"
+      p={3}
+      overflowY="auto"
+      flex="0 0 20%"
+      display={{ base: "none", md: "flex" }}
+      alignItems="center"
+      flexDirection="column"
+    >
+      <VStack spacing={4} align="stretch" width="100%">
+        {Object.entries(actions).map(([actionName, actionFunc], index) => (
+          <Button key={index} onClick={() => actionFunc()}>
+            {actionName}
+          </Button>
+        ))}
+      </VStack>
+    </Box>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -3,22 +3,23 @@ import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
 const theme = extendTheme({
-    colors: {
-        primary: "#0075F2",
-        secondary: "#272727",
-        tertiary: "#FED766",
-        grayActive1: "#414141",
-        grayActive2: "#A0AEC0",
-        grayInactive1: "#8C8C8C",
-        darkModeBackground: "#1A202C"
-    },
-    styles: {
-        global: (props) =>  ({
-            body: {
-                bg: mode("#fff", "#1A202C")(props),
-            },
-        }),
-    }
+  colors: {
+    primary: "#0075F2",
+    secondary: "#272727",
+    tertiary: "#FED766",
+    grayActive1: "#414141",
+    grayActive2: "#A0AEC0",
+    grayInactive1: "#8C8C8C",
+    darkModeBackground: "#1A202C",
+    lightModeBackground: "#f5f5f5",
+  },
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode("#f5f5f5", "#1A202C")(props),
+      },
+    }),
+  },
 });
 
 export default theme;
